c2257eaeb7421d39d2e4f3c016f8f2552b4b8b71
Dream4Theater <81992744+Dream4Theater@users.noreply.github.com>
2021-05-15 14:21:10 +0300

    first commit

---
+++ streamlit-corona-detection-app/app.py
@@ -0,0 +1,50 @@
+### Script for CS329s ML Deployment Lec 
+import SessionState
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+model = tf.keras.models.load_model('Xception.h5')
+model.compile(optimizer='adam', loss='categorical_crossentropy',metrics=['accuracy'])
+CLASSES = ['Covid','non-Covid']
+
+### Streamlit code (works as a straigtht-forward script) ###
+st.title("Corona Detection")
+@st.cache # cache the function so predictions aren't always redone (Streamlit refreshes every click)
+def make_prediction(image, model, class_names):
+    x = tf.image.decode_png(image, channels=3)
+    x = tf.image.resize(x,[128,128])
+    x /= 255.
+    x = tf.cast(tf.expand_dims(x, axis=0), tf.float32)
+
+    preds = model.predict(x, batch_size=10)
+    pred_class = class_names[tf.argmax(preds[0])]
+    pred_conf = tf.reduce_max(preds[0])
+    return image, pred_class, pred_conf
+
+# File uploader allows user to add their own image
+uploaded_file = st.file_uploader(label="Upload an image",
+                                 type=["png"])
+
+# Setup session state to remember state of app so refresh isn't always needed
+# See: https://discuss.streamlit.io/t/the-button-inside-a-button-seems-to-reset-the-whole-app-why/1051/11 
+session_state = SessionState.get(pred_button=False)
+
+# Create logic for app flow
+if not uploaded_file:
+    st.warning("Please upload an image.")
+    st.stop()
+else:
+    session_state.uploaded_image = uploaded_file.read()
+    st.image(session_state.uploaded_image, use_column_width=True)
+    pred_button = st.button("Predict")
+
+# Did the user press the predict button?
+if pred_button:
+    session_state.pred_button = True 
+
+# And if they did...
+if session_state.pred_button:
+    session_state.image, session_state.pred_class, session_state.pred_conf = make_prediction(session_state.uploaded_image, model=model, class_names=CLASSES)
+    st.write(f"Prediction: {session_state.pred_class}, \
+               Confidence: {session_state.pred_conf:.3f}")
